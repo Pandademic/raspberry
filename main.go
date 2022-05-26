bf814399169b17d77c4bc4e5e3c41b9af55a87c3
Pandademic <atharv.gupte510@gmail.com>
2022-05-26 11:01:30 -0400

    Add main file

---
+++ main.go
@@ -0,0 +1,53 @@
+package raspberry
+
+import "os"
+import "fmt"
+
+type Cli struct{
+	AcceptedCommands []string
+	HelpMsg string
+	Version float64
+}
+var(
+	Command string
+	argLength int
+)
+type fn func()
+func (c *Cli) SetHandler(cmd string,fun fn) {
+	if Command == cmd {
+		fun()
+	}	
+}
+func contains(str string,s []string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+func (c *Cli) PrintHelp(){
+	fmt.Println(c.HelpMsg)
+}
+func (c *Cli) PrintVersion(){
+	fmt.Println(c.Version)
+}
+func (c *Cli) Setup(){
+   argLength = len(os.Args)
+   if argLength > 1 {	
+	   if(contains(os.Args[1],c.AcceptedCommands)){
+		   Command = os.Args[1]
+	   }else{
+		   fmt.Println("Command not found: ",os.Args[1])
+	   }
+   }else{
+	   fmt.Println("Not enough arguments")
+	   os.Exit(1)
+   }
+   // set default cmd's
+   c.SetHandler("-v",c.PrintVersion)
+   c.SetHandler("version",c.PrintVersion)
+   c.SetHandler("-h",c.PrintHelp)
+   c.SetHandler("help",c.PrintHelp)	
+}
